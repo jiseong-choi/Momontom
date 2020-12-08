a36e47851523988b2f222ec400b1d4bf200e24b9
wonsunil <seonil6352@gmail.com>
2020-12-08 16:55:24 +0900

    Refactor: upgrade es version and change variable name

--- todo.js
+++ todo.js
@@ -1,82 +1,103 @@
-const toDoForm = document.querySelector(".js-toDoForm"),
-toDoInput = toDoForm.querySelector("input"),
-toDoList = document.querySelector(".js-toDoList");
+let todos = [];
 
-const TODOS_LS = 'toDos';
-let toDos = [];
+const deleteTodo = event => {
+    const deleteButton = event.target;
+    const todo = deleteButton.parentNode;
+    todo.parentNode.removeChild(todo);
 
-toDoForm.classList.add("form-group", "w-25");
-toDoInput.classList.add("form-control");
+    const cleanTodos = todos.filter(item => {
+        return item.id !== parseInt(todo.id);
+    });
 
-function deleteTodo(event){
-    const btn = event.target;
-    const li = btn.parentNode;
-    toDoList.removeChild(li);
-    const cleanToDos = toDos.filter(function(toDo){
-        console.log(toDo);
-        return toDo.id !== parseInt(li.id);
-    })
-    toDos = cleanToDos;
-    saveToDos();
-}
+    todos = cleanTodos;
+    saveTodos(todos);
+};
 
-function HandleCheckBox() {
-    span = this.parentNode.querySelector("span");//여기서 this는 체크박스를 가리킴 checkbox에서 parentnode에 접근을 한다음 span에 접근함.
-    if (this.checked) {
-      span.style.textDecoration = "line-through";
-    } else {
-      span.style.textDecoration = "none";
-    }
-}
+const HandleCheckBox = ({ target }) => {
+    //여기서 this는 체크박스를 가리킴
+    //checkbox에서 parentnode에 접근
+    //이후 span에 접근함.
+    const span = target.parentNode.querySelector("span");
 
-function saveToDos(){
-    localStorage.setItem(TODOS_LS,JSON.stringify (toDos))//json is javascript object 어쩌고임. javascript는 바로 로컬 스토리지에데이터를 정상적으로 저장할 수 없음
-}
+    if(target.checked) return span.style.textDecoration = "line-through";
 
-function paintToDo(text){
-    const li = document.createElement("li")
-    const delBtn = document.createElement("button");
-    const checkBox = document.createElement("input")
-    checkBox.classList.add("form-chek")
-    checkBox.type = "checkbox"
-    const newId = toDos.length +1;
-    delBtn.innerText = "X";
-    delBtn.addEventListener("click", deleteTodo)
-    const span = document.createElement("span")
+    return span.style.textDecoration = "none";
+};
+
+const saveTodos = todos => {
+    //json is javascript object 어쩌고임.
+    //javascript는 바로 로컬 스토리지에
+    //데이터를 정상적으로 저장할 수 없음
+
+    localStorage.setItem("toDos", JSON.stringify(todos));
+};
+
+const paintTodo = (text, todoList) => {
+    const todoBox = document.createElement("li")
+    const deleteButton = document.createElement("button");
+    const checkBox = document.createElement("input");
+
+    checkBox.classList.add("form-chek");
+    checkBox.type = "checkbox";
+
+    const newId = todos.length +1;
+
+    deleteButton.innerText = "X";
+    deleteButton.addEventListener("click", deleteTodo);
+
+    const span = document.createElement("span");
     span.innerText = text;
-    li.appendChild(checkBox);
-    li.appendChild(span);
-    li.appendChild(delBtn);
-    li.id = newId;
-    toDoList.appendChild(li);
-    const toDoObj = {
-        text :text,
+
+    todoBox.appendChild(checkBox);
+    todoBox.appendChild(span);
+    todoBox.appendChild(deleteButton);
+    todoBox.id = newId;
+
+    todoList.appendChild(todoBox);
+
+    const todoObject = {
         id: newId,
+        text: text,
     };
-    toDos.push(toDoObj);
-    saveToDos()
+
+    todos.push(todoObject);
+
+    saveTodos(todos);
     checkBox.addEventListener("change", HandleCheckBox);
-}
+};
 
-function handleSubmit(event) {
+const handleSubmit = event => {
     event.preventDefault();
-    const currentValue = toDoInput.value;
-    paintToDo(currentValue);
-}
 
-function loadToDos(){
-    const loadedToDos = localStorage.getItem(TODOS_LS);
-    if (loadedToDos !== null){
-        const parsedToDos = JSON.parse(loadedToDos);
-        parsedToDos.forEach(function (toDo){
-        paintToDo(toDo.text)
-    })
-    }
+    const todoInput = event.target.children[0];
+    const todoList = document.querySelector(".js-toDoList");
+
+    const currentValue = todoInput.value;
+    paintTodo(currentValue, todoList);
+
+    todoInput.value = "";
 };
 
-function init() {
-    loadToDos();
-    toDoForm.addEventListener("submit", handleSubmit);
+const loadTodos = _ => {
+    const todoList = document.querySelector(".js-toDoList");
+    const loadedTodos = localStorage.getItem("toDos");
+
+    if(loadedTodos === null || loadedTodos === "undefined") return false;
+    
+    const parsedTodos = JSON.parse(loadedTodos);
+
+    parsedTodos.forEach(todo => {
+        paintTodo(todo.text, todoList);
+    });
 };
 
-init();
+window.onload = _ => {
+    const todoForm = document.querySelector(".js-toDoForm");
+    const todoInput = todoForm.querySelector("input");
+    
+    todoForm.classList.add("form-group", "w-25");
+    todoInput.classList.add("form-control");
+    
+    loadTodos("toDos");
+    todoForm.addEventListener("submit", handleSubmit);
+};
